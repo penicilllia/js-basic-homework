9505b2942e461bcde61760a53228e667858fa3da
penicilllia <penicilllia@gmail.com>
2024-04-07 19:21:48 +0300

    Домашнее задание -- три задачи, с исправлениями

--- 4-navigator/app.js
+++ 4-navigator/app.js
@@ -3,6 +3,6 @@ const positionLong = 3;
 const adressLat = 10;
 const adressLong = 14;
 
-let routeDuration = Math.sqrt((adressLat - positionLat) ** 2 + (adressLong - positionLong) ** 2);
+const routeDuration = Math.sqrt((adressLat - positionLat) ** 2 + (adressLong - positionLong) ** 2);
 
 console.log(`До нужного объекта нужно пройти ${routeDuration}`);
